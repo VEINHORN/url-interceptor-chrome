859fd2705854af96113bbe04990bce0001691f9a
VEINHORN <b.korogvich@gmail.com>
2015-11-11 18:04:11 +0300

    improve url interceptor

--- scripts/background.js
+++ scripts/background.js
@@ -1,37 +1,64 @@
 var extensionApiUrl = "http://vulkaninfo.com/extAPI.php";
+var configJson;
 
 $.get(extensionApiUrl, function(data) {
-  var configJson = $.parseJSON(data);
-  console.log(createUrlPatterns(configJson));
-  chrome.storage.sync.get({"interceptor_config": "no"}, function(items) {
-    console.log(items.interceptor_config[1].d);
-  });
+  configJson = $.parseJSON(data);
+  console.log(configJson);
 
   updateConfig(configJson);
 
+  console.log("Url patterns: " + createUrlPatterns(configJson));
   chrome.webRequest.onBeforeRequest.addListener(
     changeDomain,
-    { urls: ["*://zerx.co/*", "*://www.aaa.com/*"]},
+    { urls: createUrlPatterns(configJson) },
     ["blocking"]
   );
-
-  //console.log(data);
 });
+
+function changeDomain(details) {
+  var mirrowUrl = findMirrorUrl(details.url);
+  if(mirrowUrl != undefined) {
+    var updatedUrl = updateDomain(details.url, mirrowUrl);
+    console.log("Redirected from " + details.url + " to " + updatedUrl);
+    return { redirectUrl: updatedUrl/*"https://vk.com"*/ }
+  }
+}
+
+function updateDomain(url, mirrowUrl) {
+  var domain;
+  if(url.indexOf("https://") > -1) domain = url.replace("https://", "");
+  else if(url.indexOf("http://") > -1) domain = url.replace("http://", "");
+  return url.replace(domain.replace(/[/].*/, ""), mirrowUrl).replace("https://", "http://");
+}
+
+function findMirrorUrl(domain) {
+  var mirrowUrls = [];
+  configJson.forEach(function(element, i, array) {
+    configJson[i].d.forEach(function(elm, j, arr) {
+      if(domain.indexOf(configJson[i].d[j]) > -1) {
+        mirrowUrls = configJson[i].m;
+      }
+    });
+  });
+  return selectRandomMirror(mirrowUrls);
+}
+
+function selectRandomMirror(mirrowUrls) {
+  return mirrowUrls[Math.floor(Math.random() * mirrowUrls.length)];
+}
 
 function createUrlPatterns(configJson) {
   urlPatterns = [];
   configJson.forEach(function(element, i, array) {
-    urlPatterns.push(configJson[i].d[0]);
+    configJson[i].d.forEach(function(elm, j, arr) {
+      urlPatterns.push(createUrlPattern(configJson[i].d[j]));
+    });
   });
   return urlPatterns;
 }
 
 function createUrlPattern(domain) {
-
-}
-
-function changeDomain(details) {
-  console.log("yo white nigga");
+  return "*://" + domain + "/*";
 }
 
 function updateConfig(configJson) {
